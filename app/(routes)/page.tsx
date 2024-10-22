feecbb1d5bcfd6187f76094d194a20c0e4718021
Nicoj3000 <nicoj3000its@gmail.com>
2024-10-22 09:57:47 -0500

    linkchat

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <p className="text-muted-foreground mb-6">
           ChatBot is a tool that you ask for any question and it will answer you.
         </p>
-        <Link target="_blank" href="https://ui.shadcn.com/">
+        <Link target="_blank" href="http://localhost:8501/">
           <span className="inline-block px-6 py-3 bg-primary text-primary-foreground font-semibold rounded-lg shadow-md hover:bg-primary/80 transition duration-300 cursor-pointer">
             Get Started
           </span>
